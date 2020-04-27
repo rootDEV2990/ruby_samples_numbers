229f94958c1c8c6dd812f43f6e401e271fe595d0
rootDEV2990 <website.dev@icloud.com>
2020-04-27 11:21:38 -0500

    Added examples of numbers with basic opperators in ruby

---
+++ numbers.rb
@@ -0,0 +1,21 @@
+# Addition
+1 + 1
+
+# Subtraction
+p 2 - 1
+
+# Multiplication
+p 2 * 2
+
+# Division
+p 10 / 5 
+
+# Exponents
+p 2 ** 2
+p 3 ** 3
+
+# Modulus (find the remainder of devision)
+p "8/2=4; no remainder"
+8 % 2
+p "10/4=2 with a remainder of 2"
+10 % 4
